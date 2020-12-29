a16ce1517ec2cadb21f1b2e6e6f409a9746fb0b5
MultiSys-temp <76628633+MultiSys-temp@users.noreply.github.com>
2020-12-29 04:53:54 +0200

    first upload

---
+++ MULTISYSDbContext/Models/DatabaseContext.cs
@@ -0,0 +1,57 @@
+﻿using SQLite.CodeFirst;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Core.Common;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Infrastructure.DependencyResolution;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.SQLite;
+using System.Data.SQLite.Linq;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace MULTISYSDbContext.Models
+{
+
+    [DbConfigurationType(typeof(SQLiteConfiguration))]
+    //[DbConfigurationType("MULTISYSPlugin.SQLiteConfiguration, MULTISYSPlugin2021")]
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() :
+            base(new SQLiteConnection()
+            {
+                ConnectionString = new SQLiteConnectionStringBuilder() { DataSource = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "BIMGO.sqlite"), ForeignKeys = true}.ConnectionString
+            }, true)
+        {
+            //Database.CreateIfNotExists();
+
+        }
+
+        /*public DatabaseContext() :
+            base("Data Source=BIMGO.db; Version=3;")
+        {
+            //Database.CreateIfNotExists();
+
+        }*/
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+
+            var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<DatabaseContext>(modelBuilder);
+            Database.SetInitializer(sqliteConnectionInitializer);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        
+
+        public DbSet<LOT> LOTs { get; set; }
+    }
+}
---
+++ MULTISYSDbContext/Models/LOT.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Linq.Mapping;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MULTISYSDbContext.Models
+{
+    [Table(Name = "LOTs")]
+    public class LOT
+    {
+        public LOT()
+        {
+
+        }
+
+        [Column(Name = "Id", IsDbGenerated = true, IsPrimaryKey = true, DbType = "INTEGER")]
+        [Key]
+        public int Id { get; set; }
+
+        [Column(Name = "EmpName", DbType = "VARCHAR")]
+        public string Name { get; set; }
+
+        [Column(Name = "Color")]
+        public int Color { get; set; }
+
+        [Column(Name = "Description", DbType = "VARCHAR")]
+        public string Description { get; set; }
+
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
